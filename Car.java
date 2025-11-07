71e32743345c654310d6b5f76776970d226acfc9
MUNNANOORUPAVANI <pavanimunnanooru@gmail.com>
2025-11-08 02:17:30 +0530

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,18 +1,14 @@
-public class Car{
-    //variables
-    int speed;
-    String model;
+package com.avn.tns.oopsdemo;
 
-    //Methods
-    void start(){
-        System.out.println("The car starts moving");
-    }
-    //Main method
-    public static void main(String[] args){
-        Car c= new Car();
-        c.model="Honda";
-        c.speed=180;
-        c.start();
-    }
+public class Car extends Vehicle {
+	public int seatingCapacity;
+	public Car(String vehicleName,String vehicleColor,int seatingCapacity) {
+		super(vehicleName,vehicleColor);
+		this.seatingCapacity=seatingCapacity;
+	}
+	public void display() {
+		super.display();
+		System.out.println("seatingcapacity ="+seatingCapacity);
+	}
+	
 }
-
